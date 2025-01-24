ca3045c8e6432f7c27558ba52fe1927486b2675f
FlamestarRS <FlamestarRS@gmail.com>
2025-01-24 16:12:41 -0500

    added rotation

--- main.py
+++ main.py
@@ -24,10 +24,13 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        player.update(dt)
         screen.fill((0,0,0))
-
         player.draw(screen)
-
         pygame.display.flip()
 
         clock.tick(60)
