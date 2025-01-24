f3cd3a260322d49134dd4137b583cb9727a7a767
FlamestarRS <FlamestarRS@gmail.com>
2025-01-24 15:25:00 -0500

    60fps cap, created circleshape.py

--- main.py
+++ main.py
@@ -6,16 +6,24 @@ import pygame
 from constants import *
 
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 def main():
     pygame.init()
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         screen.fill((0,0,0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60) / 1000)
         
 
 if __name__ == "__main__":
